4a2f9b6ed7cf0073481c5cb601707a08bac2fdc1
Rain <rainkalugdan@gmail.com>
2026-02-02 13:12:32 +0800

    feat(error-state): refetch data when user clicks on retry

--- src/components/ui/ErrorState.tsx
+++ src/components/ui/ErrorState.tsx
@@ -1,3 +1,6 @@
+import { AlertCircle } from "lucide-react";
+import { useProfilesContext } from "../../contexts/ProfilesContext";
+
 interface ErrorStateProps {
   heading?: string;
   message?: string;
@@ -5,12 +8,22 @@ interface ErrorStateProps {
 
 export default function ErrorState({
   heading = "Unable to Load Profiles",
-  message = "An error occured. Please try again later.",
+  message = "An error occurred. Please try again later.",
 }: ErrorStateProps) {
+  const { refetch } = useProfilesContext();
+
   return (
-    <div>
-      <h2>{heading}</h2>
-      <p>{message}</p>
+    <div className="flex flex-col justify-center items-center gap-2 py-16 border border-2 border-pink-200 rounded">
+      <AlertCircle className="w-12 h-12 mb-2 text-pink-500" />
+      <h2 className="text-xl font-bold">{heading}</h2>
+      <p className="mb-2 text-center text-gray-900">{message}</p>
+
+      <button
+        onClick={() => refetch}
+        className="px-6 py-2 bg-white border-2 border-pink-500 text-pink-600 font-bold rounded-lg hover:bg-gradient-to-r hover:from-pink-500 hover:to-rose-500 hover:text-white hover:border-transparent transition-all cursor-pointer"
+      >
+        Retry
+      </button>
     </div>
   );
 }
