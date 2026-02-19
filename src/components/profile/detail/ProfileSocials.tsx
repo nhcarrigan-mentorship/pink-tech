36b51f74f80e3845daed721706e47e826207dbd6
Rain <rainkalugdan@gmail.com>
2026-02-19 16:11:29 +0800

    feat(profile-socials): remove icons, labels, and add names

--- src/components/profile/detail/ProfileSocials.tsx
+++ src/components/profile/detail/ProfileSocials.tsx
@@ -1,24 +1,17 @@
-import LazyIcon from "../../ui/LazyIcon";
-
-const socialClassName = "w-5 h-5 text-pink-600";
-
 export const socials = [
   {
     key: "linkedin",
-    label: "Linkedin",
+    name: "Linkedin",
     placeholder: "https://www.linkedin.com/in/username",
-    icon: <LazyIcon name="Linkedin" className={socialClassName} />,
   },
   {
     key: "twitter",
-    label: "Twitter",
+    name: "Twitter",
     placeholder: "@yourusername",
-    icon: <LazyIcon name="Twitter" className={socialClassName} />,
   },
   {
     key: "website",
-    label: "website",
+    name: "Globe",
     placeholder: "https://yourwebsite.com",
-    icon: <LazyIcon name="Globe" className={socialClassName} />,
   },
 ];
