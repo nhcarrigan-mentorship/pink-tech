0b975317a640689f520046dade09e75dd8b4c537
Rain <rainkalugdan@gmail.com>
2026-02-24 16:42:01 +0800

    refactor(profile-image): set error image background to white

--- src/components/ui/ImageWithFallback.tsx
+++ src/components/ui/ImageWithFallback.tsx
@@ -24,7 +24,7 @@ export default function ImageWithFallback(
 
   return didError ? (
     <div
-      className={`inline-block bg-gray-100 text-center align-middle ${className ?? ""}`}
+      className={`inline-block bg-white text-center align-middle ${className ?? ""}`}
       style={style}
     >
       <div className="flex items-center justify-center w-full h-full">
