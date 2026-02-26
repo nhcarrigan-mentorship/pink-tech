39d024c9a7e8226966a0e5855a1ab83ff6cee285
Rain <rainkalugdan@gmail.com>
2026-02-26 16:33:24 +0800

    feat(email-verification-notice): add structure

---
+++ src/components/auth/EmailVerificationNotice.tsx
@@ -0,0 +1,30 @@
+import LazyIcon from "../ui/LazyIcon";
+
+interface EmailVerificationNoticeProps {
+  email: string;
+}
+
+export default function EmailVerificationNotice({
+  email,
+}: EmailVerificationNoticeProps) {
+  return (
+    <div className="bg-white p-8 border border-pink-100 rounded-2xl shadow-xl">
+      <div className="text-center mb-8">
+        <div className="inline-flex justify-center items-center w-16 h-16 mb-4 bg-gradient-to-br from-pink-500 to-rose-500 text-white rounded-full">
+          <LazyIcon name="Mail" className="w-8 h-8 text-white" />
+        </div>
+        {/* Header */}
+        <h1 className="text-3xl font-bold mb-2">Verify Your Email</h1>
+        <p className="text-gray-600">
+          We have sent a verification link to{" "}
+          <span className="font-bold">{email}</span>. <br></br> <br></br>Click
+          on the link to complete the verification process. <br></br>You might
+          need to check your spam folder.{" "}
+        </p>
+        <button className="w-80 py-3 mt-8 rounded-lg bg-gradient-to-r from-pink-500 to-rose-500 text-white font-bold cursor-pointer hover:from-pink-600 to-rose-600 hover:scale-105 shadow-xl disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:scale-100">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/auth/Verify.tsx
+++ src/pages/auth/Verify.tsx
@@ -1,3 +1,22 @@
+import { useLocation } from "react-router-dom";
+import EmailVerificationNotice from "../../components/auth/EmailVerificationNotice";
+
 export default function Verify() {
-  return <div>Verify Email</div>;
+  const location = useLocation();
+    const state = location.state as any;
+    const email = state
+      ? typeof state === "string"
+        ? state
+        : state.email ?? localStorage.getItem("pendingEmail") ?? ""
+      : localStorage.getItem("pendingEmail") ?? "";
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-50 via-pink-100 to-rose-50">
+      <div className="flex-1 flex justify-center items-center px-4 py-12">
+        <div className="w-full max-w-md">
+          <EmailVerificationNotice email={email} />
+        </div>
+      </div>
+    </div>
+  );
 }
