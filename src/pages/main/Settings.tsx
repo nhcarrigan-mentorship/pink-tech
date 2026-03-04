48f39d48c4ac17b6c2d059b3b36ff7b3a14582d8
Rain <rainkalugdan@gmail.com>
2026-03-04 16:49:06 +0800

    feat(settings): add DeleteAccount component

--- src/pages/main/Settings.tsx
+++ src/pages/main/Settings.tsx
@@ -1,5 +1,11 @@
 import AccountSettings from "../../components/profile/settings/AccountSettings";
+import DeleteAccount from "../../components/profile/settings/DeleteAccount";
 
 export default function Settings() {
-  return <AccountSettings />;
+  return (
+    <div className="py-8 md:py-16">
+      <AccountSettings />
+      <DeleteAccount />
+    </div>
+  );
 }
