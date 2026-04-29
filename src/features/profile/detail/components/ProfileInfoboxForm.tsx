248b7501998ad14d9841728111d4d60aa33ff54b
Rain <rainkalugdan@gmail.com>
2026-04-29 16:45:54 +0800

    fix(profile-info-form): set website label to website

--- src/features/profile/detail/components/ProfileInfoboxForm.tsx
+++ src/features/profile/detail/components/ProfileInfoboxForm.tsx
@@ -540,7 +540,7 @@ export default function ProfileInfoboxForm({
             {socials.map(({ key, placeholder, name }) => (
               <div key={key} className="flex flex-col gap-2">
                 <div className="flex items-center gap-2">
-                  <label htmlFor="role">
+                  <label htmlFor="website">
                     <span className="hidden">{key}</span>
                     <LazyIcon
                       name={name}
